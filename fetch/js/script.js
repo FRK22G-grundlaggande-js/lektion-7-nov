ee6efd79ff4175fe4fc4f048206d45ab83b79979
Johan Kivi <johan.kivi@zocom.se>
2022-11-07 15:01:53 +0100

    mmm

--- fetch/js/script.js
+++ fetch/js/script.js
@@ -6,26 +6,37 @@ const main = document.querySelector('main');
 btn.addEventListener('click', async function() {
     // Vid klick
     // 1. Bygg ihop URL
+    try {
     const base = 'https://swapi.dev/api';
-    const resource = 'planets';
-    const id = 1;
+    const resource = document.querySelector('select').value;
+    const id = document.querySelector('input').value;
     const url = `${base}/${resource}/${id}`;
 
     // 2. Anropa fetchData-funktionen med URL
     main.innerHTML = 'loading...'
     const data = await fetchData(url);
-    console.log(data);
+
     // 3. Gör något med svaret
     updateDOM(data);
+
+    // Om något gick fel, fånga upp
+    } catch(err){ // Fånga up ev fel i await funktionerna, ex. hittar ej resursen
+        console.error(err)
+        main.innerHTML = err; // visa för användare att något sket sig
+    }
 })
 
 
 // Vid klick, anropa servern
 async function fetchData(url){
-    const resp = await fetch(url)
-    const data = await resp.json();
-    return data;
-}  
+        const resp = await fetch(url);
+        const data = await resp.json();
+        console.log(data);
+        if(data.detail === 'Not found'){
+           throw Error(data.detail) // Kasta ett fel!
+        };
+        return data;
+}
 
 
 function updateDOM(data){
@@ -55,6 +66,19 @@ function updateDOM(data){
         <article>
             <h1>${data.name}</h1>
             <p>diameter: ${data.diameter}</p>
+        </article>
+        `
+    }
+
+    // Is it a starship?
+    if(data.starship_class){
+
+        console.log('Its a starship!')
+
+        template = `
+        <article>
+            <h1>${data.name}</h1>
+            <p>Class: ${data.starship_class}</p>
         </article>
         `
     }
